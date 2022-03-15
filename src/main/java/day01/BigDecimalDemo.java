197da5a6ea010e5be7d6bbd1dd798513af1b8ca4
MB-study <MB-study@MB-205-6>
2022-03-15 21:49:00 +0800

    ocp

---
+++ src/main/java/day01/BigDecimalDemo.java
@@ -0,0 +1,20 @@
+
+package day01;
+
+import java.math.BigDecimal;
+
+
+public class BigDecimalDemo {
+    public static void main(String[] args) {
+        System.out.println(0.1 + 0.1 == 0.2);
+        System.out.println(0.1 + 0.1 + 0.1 == 0.3);
+        System.out.println(0.1 + 0.1);
+        System.out.println(0.1 + 0.1 + 0.1);
+        BigDecimal a = new BigDecimal("0.1");
+        BigDecimal b = new BigDecimal("0.1");
+        BigDecimal c = new BigDecimal("0.1");
+        System.out.println(a.add(b).add(c).doubleValue());
+        System.out.println(a.add(b).add(c).doubleValue() == 0.3);
+        
+    }
+}
